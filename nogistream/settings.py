a11403b0e4ba5626a866c226392067b7f2f740f5
chivequiachon <chivequiachon@gmail.com>
2019-04-26 11:43:07 +0800

    Fixed cloudinary env var

--- nogistream/settings.py
+++ nogistream/settings.py
@@ -133,9 +133,11 @@ STATICFILES_DIRS = (
 
 django_heroku.settings(locals())
 
+CLOUDINARY_NAME = os.environ['CLOUDINARY_NAME']
+
 # Set cloudinary
 cloudinary.config( 
-  cloud_name = os.environ['CLOUDINARY_NAME'],
+  cloud_name = CLOUDINARY_NAME,
   api_key = os.environ['CLOUDINARY_API_KEY'],
   api_secret = os.environ['CLOUDINARY_API_SECRET'] 
 )
